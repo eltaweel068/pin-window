fa6f863c4302defff42cd7fafd091b3bc215dfa5
Meko <mohamedeltaweel068@gmail.com>
2026-05-08 06:40:11 +0300

    Change window pinning shortcut to Control+Super+Z

--- install.sh
+++ install.sh
@@ -15,4 +15,4 @@ echo
 echo "Next steps:"
 echo "  1. Log out and back in (or on Xorg: Alt+F2, type 'r', Enter)."
 echo "  2. Enable:   gnome-extensions enable $UUID"
-echo "  3. Focus a window and press Super+P to pin it."
+echo "  3. Focus a window and press Control+Super+Z to pin it."
